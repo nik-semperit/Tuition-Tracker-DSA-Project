1ba62d9e131ce5a9703c2d35028b6e32a6b4c4c0
nik-semperit <rc24116@adab.umpsa.edu.my>
2026-01-01 10:36:40 +0800

    Update main.java

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Main Menu: 1") 
+        System.out.println("Main Menu: 1");
         System.out.println("Please make a choice: ");
 
 
